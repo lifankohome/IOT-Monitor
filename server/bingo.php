46e7845d6083bdab303bbb8f55e83228a81de3ea
lifankohome <lifankohome@163.com>
2017-12-21 21:40:46 +0800

    IOT Server Code

--- server/bingo.php
+++ server/bingo.php
@@ -7,34 +7,34 @@
  */
 header("Content-type: text/html;charset=utf-8");
 
-$serverName = "localhost";
-$dbName = "lifanko";
-$dbUserName = "root";
-$dbPassword = "lifanko521";
 $tableName = "monitor";
 
 try {   //创建pdo连接对象，全局使用
-    $pdo = new PDO("mysql:host=$serverName;dbname=$dbName", "$dbUserName", "$dbPassword");
+    $pdo = new PDO("mysql:host=localhost;dbname=lifanko", "root", "");
 } catch (PDOException $e) {
-    die("Unable to connect to the server, please contact manager: lifankohome@163.com");
+    die("Unable to connect to the database, please contact manager: lifankohome@163.com");
 }
 $pdo->query("set names utf8");
 
-function getValue($index)
+function getValue($index, $default = '')
 {
-    if (isset($_GET[$index]) && !empty($_GET[$index])) {
-        return $_GET[$index];
+    if (!empty($_GET[$index])) {
+        return round($_GET[$index], 1); //至多保留一位小数+去数字前无用的0
     } else {
-        return '';
+        return $default;
     }
 }
 
-$opt = getValue('opt'); //查询选项
-$uid = getValue('uid'); //唯一标识
+if (isset($_GET['mac']) && strlen($_GET['mac']) == 17) {
+    $uid = $_GET['mac']; //唯一标识
+} else {
+    die('No MAC Given');
+}
 
-$temp = getValue('T');  //温度
-$humi = getValue('H');  //湿度
-$smog = getValue('S');  //颗粒物浓度
+$opt = getValue('opt'); //查询选项
+$temp = getValue('T', 0);  //温度
+$humi = getValue('H', 0);  //湿度
+$smog = getValue('S', 0);  //颗粒物浓度
 
 if ($opt == 'mon') {    //mon(itor)
     $sql = "SELECT time FROM $tableName WHERE uid='{$uid}' ORDER BY id DESC limit 1";
@@ -54,9 +54,11 @@ if ($opt == 'mon') {    //mon(itor)
                 echo 'FAIL!';
             }
         } else {
-            echo $presentTime - $monitorLastTime;
+            echo $presentTime - $monitorLastTime;   //剩余保留时间
         }
     } else {  //未查询到上一条数据，说明该设备是新设备，进行初始化操作
+        $sql = "INSERT INTO monitor_home (uid, time) VALUES ('{$uid}', '{$presentTime}')"; //插入新的数据
+        $stmt = $pdo->query($sql);
         $sql = "INSERT INTO $tableName (uid, temp, humi, smog, time) VALUES ('{$uid}', '{$temp}', '{$humi}', '{$smog}', '{$presentTime}')"; //插入新的数据
         $stmt = $pdo->query($sql);
         if ($stmt->rowCount() > 0) {
