dd549b1e6ffd33e4fd6e7618b5e013773e8d95a4
lifankohome <lifankohome@163.com>
2017-12-22 18:45:26 +0800

    Add: DHT11 Sensor On P21

--- mcu/iot-monitor.c
+++ mcu/iot-monitor.c
@@ -5,22 +5,31 @@
 #define S2TI  0x02
 #define S2RI  0x01
 
-sbit Beep = P0^1;
+sbit relay = P2^6;
 
-#define PIN_18B20 P35  //DS18B20通信引脚
+#define PIN_18B20 P24  //DS18B20通信引脚
+#define DHT P21		//湿度传感器引脚
 
+code uchar cmdIP[] = "AT+CIFSR\r\n";
 code uchar cmdAiLink[] = "AT+CWSMARTSTART=0\r\n";	//AiLink
 code uchar cmdMac[] = "AT+CIPSTAMAC?\r\n";	//获取MAC
 code uchar cmdRunMode[] = "AT+CWMODE_DEF=1\r\n";	//Station模式
 code uchar cmdConMode[] = "AT+CIPMUX=0\r\n";	//单连接模式
-code uchar cmdTCP[] = "AT+CIPSTART=\"TCP\",\"eeec.hpu.edu.cn\",7103\r\n";	//建立TCP连接
+code uchar cmdTCP[] = "AT+CIPSTART=\"TCP\",\"hpu.lifanko.cn\",81\r\n";	//建立TCP连接
 code uchar cmdTCPLen[] = "AT+CIPSEND=100\r\n";	//TCP请求长度
-uchar cmdTCPURL[] = "GET /index.php?tip=mon&mac=00-00-00-00-00-00&T=00.0&S=000&H=000 HTTP/1.1\r\n Host: eeec.hpu.edu.cn\r\n\r\n";	//发起请求
+uchar cmdTCPURL[] = "GET /bingo.php?opt=mon&mac=00-00-00-00-00-00&T=00.0&S=0000&H=00 HTTP/1.1\r\n Host: eeec.hpu.edu.cn\r\n\r\n";	//发起请求
 
-uchar String[245] = "";	
-uchar posString = 0, lenString = 0;
+uchar A4[16] = {0};
+uchar A4D0 = 0,A4D1 = 0;
+
+uchar String[245] = "";
+uchar posString = 0, lenString = 0, A4Pos = 0;
 bit sendFlag = 0, connected = 0;  //发送完毕标识符，已连接标识符
 uchar msgT = 0, failT = 0;
+
+unsigned int restart = 0;
+
+unsigned int total = 0;	   //颗粒物浓度
 
 void UartInit(void);
 void Uart2Init(void);
@@ -43,15 +52,20 @@ void smartLink();
 bit Start18B20(void);
 bit Get18B20Temp(unsigned int *temp);
 
+uchar dht11();
+
+void A4Data();
+
 void main(){
 	UartInit();
 	Uart2Init();
 	Timer0Init();
-	Beep = 0;	//开启WiFi
+	delayMs(20);
+	Start18B20();
 	while(!connected){
-		delayMs(20);//延时2S，开机获取IP
+		sendAll(cmdIP);
+		delayMs(20);delayMs(20);//延时4S，开机获取IP
 	}
-	
 	getMac();	//获取MAC
 	sendAll(cmdRunMode);//发送命令
 	delayMs(10);//延时1S
@@ -61,22 +75,95 @@ void main(){
 	delayMs(5);//延时0.5S
 	while(1){
 		getData();
-		delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20);
-		delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20); //延时20s
-		S2BUF = failT+'0';DelayCharUsart();
+		delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20); //延时10s
+        delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20); //延时10s
+        delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20); //延时10s
+        delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20); //延时10s
+        delayMs(20);delayMs(20);delayMs(20);delayMs(20);delayMs(20); //延时10s
+        delayMs(20);delayMs(20);delayMs(20);delayMs(20);             //延时7s
 		if(connected){
 			failT++;
-			if(failT >= 5){	//5次查询未接收到大于200个字符的信息则进行重启
+			restart++;
+			if(failT > 5 || restart > 720){	//5次查询未接收到字符重启
 				connected = 0;	//改变连接状态
-				Beep = 1;
+				failT = 0;
+				restart = 0;
+
+				relay = 0;
 				delayMs(20);
-				Beep = 0;
+				relay = 1;
 			}
 		}
 		while(!connected){
 			delayMs(20);//延时2S，开机获取IP
 		}
 	}
+}
+void A4Data(){
+	total = A4D1 * 256 + A4D0;
+	if(total>1000){
+		;	  //BUG，用空操作覆盖
+	}else if(total>999){
+//		cmdTCPURL[54] = total/1000+'0';
+//		cmdTCPURL[55] = total/100%10+'0';
+//		cmdTCPURL[56] = total/10%10+'0';
+//		cmdTCPURL[57] = total%10+'0';
+		;	  //BUG，用空操作覆盖
+	}else if(total>99){
+		cmdTCPURL[54] = '0';
+		cmdTCPURL[55] = total/100%10+'0';
+		cmdTCPURL[56] = total/10%10+'0';
+		cmdTCPURL[57] = total%10+'0';
+	}else if(total>9){
+		cmdTCPURL[54] = '0';
+		cmdTCPURL[55] = '0';
+		cmdTCPURL[56] = total/10%10+'0';
+		cmdTCPURL[57] = total%10+'0';
+	}else{
+		cmdTCPURL[54] = '0';
+		cmdTCPURL[55] = '0';
+		cmdTCPURL[56] = '0';
+		cmdTCPURL[57] = total%10+'0';
+	}
+}
+uchar dht11(){
+	uchar dhtTemp = 0;
+	uchar dhtData = 0;
+	uchar i = 0;
+	uchar expire = 2;
+
+	DHT = 0;
+	delay_us(20000);
+	DHT = 1;
+	delay_us(40);
+
+	if(!DHT){
+		while(!DHT && expire++);
+		expire = 2;
+		while(DHT && expire++);
+		expire = 2;
+
+		for(i=0;i<8;i++) {
+			while(!DHT && expire++);
+			expire = 2;
+
+			delay_us(50);
+
+			if(DHT){
+				dhtTemp = 1;
+			}else{
+				dhtTemp = 0;
+			}
+
+			while(DHT && expire++);
+			if(expire==1) break;	//error happen
+
+			dhtData<<=1;
+			dhtData |= dhtTemp;
+		}
+	}
+
+	return dhtData;
 }
 void smartLink(){	//启动智能连接
 	connected = 0;
@@ -87,47 +174,80 @@ void getData(){		//模块查询数据
 	uchar n = 0;
 	unsigned int intT, decT, temp;
 	uchar Temp[4];
+	uchar dhtBuffer;
 
 	Start18B20();
 
 	if(Get18B20Temp(&temp)){
-		intT = temp >> 4;
-		decT = temp & 0xF;
-		decT = (decT*1000) / 16;
-	}
-	if(intT>9){
-		Temp[0] = '0'+intT/10;
-		Temp[1] = '0'+intT%10;
-		if(decT>99){
-			Temp[2] = '.';
-			Temp[3] = '0'+decT/100;
-		}else if(decT>9){
-			Temp[2] = '.';
-			Temp[3] = '0'+decT/10;
+		if(temp>0xF000){
+			temp = ~temp + 1;
+
+			intT = temp >> 4;		//乘0.0625,得到温度
+			decT = temp & 0xF;		//获取低四位（小数部分）
+			decT = ((decT * 100) / 16) + 0.5;
+
+			if(intT>9){
+				Temp[0] = '-';
+				Temp[1] = '0' + intT/10;
+				Temp[2] = '0' + intT%10;
+				Temp[3] = '.';
+			}else{
+				Temp[0] = '-';
+				Temp[1] = '0'+intT;
+				if(decT>9){
+					Temp[2] = '.';
+					Temp[3] = '0' + decT/10;
+				}else{
+					Temp[2] = '.';
+					Temp[3] = '0';
+				}
+			}
 		}else{
-			Temp[2] = '.';
-			Temp[3] = '0'+decT;
-		}
-	}else{
-		Temp[0] = '0'+intT%10;
-		if(decT>99){
-			Temp[1] = '.';
-			Temp[2] = '0'+decT/100;
-			Temp[3] = '0'+decT/10;
-		}else if(decT>9){
-			Temp[1] = '.';
-			Temp[2] = '0'+decT/10;
-			Temp[3] = '0'+decT;
-		}else{
-			Temp[1] = '.';
-			Temp[2] = '0'+decT;
-			Temp[2] = '0';
+			intT = temp >> 4;		//乘0.0625,得到温度
+			decT = temp & 0xF;		//获取低四位（小数部分）
+			decT = ((decT * 100) / 16) + 0.5;
+
+			if(intT>9){
+				Temp[0] = '0'+intT/10;
+				Temp[1] = '0'+intT%10;
+				if(decT>9){
+					Temp[2] = '.';
+					Temp[3] = '0'+decT/10;
+				}else{
+					Temp[2] = '.';
+					Temp[3] = '0';
+				}
+			}else{
+				Temp[0] = '0' + intT%10;
+				if(decT>9){
+					Temp[1] = '.';
+					Temp[2] = '0'+decT/10;
+					Temp[3] = '0'+decT%10;
+				}else{
+					Temp[1] = '.';
+					Temp[2] = '0';
+					Temp[3] = '0'+decT;
+				}
+			}
 		}
 	}
-	cmdTCPURL[47] = Temp[0];
+
+	cmdTCPURL[47] = Temp[0];	//填入温度
 	cmdTCPURL[48] = Temp[1];
 	cmdTCPURL[49] = Temp[2];
 	cmdTCPURL[50] = Temp[3];
+
+	dhtBuffer = dht11();
+
+	if(dhtBuffer>9){
+		cmdTCPURL[61] = dhtBuffer/10 + '0';
+		cmdTCPURL[62] = dhtBuffer%10 + '0';
+	}else{
+		cmdTCPURL[61] = '0';
+		cmdTCPURL[62] = dhtBuffer%10 + '0';
+	}
+
+	A4Data(); 					//填入空气质量
 
 	delayMs(5);//延时0.5S
 	sendAll(cmdTCP);//发送命令
@@ -136,15 +256,17 @@ void getData(){		//模块查询数据
 	delayMs(5);//延时0.5S
 	sendAll(cmdTCPURL);//发送命令
 	delayMs(10);//延时1S
-	if(lenString>200){
-		failT = 0;		//收到超过200个字符的信息就将失败次数置零
-	}	
-	/////////////////////////////String[236]:off[_]on[o]	
+
+	if(lenString>10){
+		failT = 0;		//收到超过10个字符的信息就将失败次数置零
+		lenString = 0;
+	}
+
 }
 void getMac(){ 	//获取模块Mac地址
 	uchar i = 0;
 	sendAll(cmdMac);//发送命令
-	delayMs(10);//延时1S		
+	delayMs(10);//延时1S
 	for(i=0;i<lenString;i++){
 		if(String[i]=='M'){
 			if(String[i+1]=='A'){
@@ -168,7 +290,7 @@ void getMac(){ 	//获取模块Mac地址
 						cmdTCPURL[42] = String[20+i];
 						cmdTCPURL[43] = String[21+i];
 
-						break;		
+						break;
 					}
 				}
 			}
@@ -203,7 +325,7 @@ void checkStr(){	//检查是否连接网络成功，检查收到的字符数目
 	numUsart(posString);	//显示输入的字符数目
 	lenString = posString;
 	posString = 0;
-	
+
 	if(!connected){
 		if(lenString==13){
 			for(i=0;i<lenString;i++){
@@ -214,13 +336,19 @@ void checkStr(){	//检查是否连接网络成功，检查收到的字符数目
 								if(String[i+5]=='P'){
 									connected = 1;	//连接网络成功
 								}
-							}	
+							}
 						}
 					}
 				}
 			}
+		}else{		//应对单片机重启后状况
+			if(lenString==76){
+				connected = 0;	//+CIFSR:STAIP,"0.0.0.0"
+			}else if(lenString>80){
+				connected = 1;	//+CIFSR:STAIP,"192.168.0.110"
+			}
 		}
-	}	
+	}
 }
 void numUsart(uchar num){	//将串口1收到的字符数目通过串口2发出来
 	uchar ge = 0, shi = 0, bai = 0;
@@ -262,11 +390,11 @@ void UartInit(void){		//4800bps@32.0000MHz
 void USART() interrupt 4{
 	if(TI){
 		TI = 0;
-		sendFlag = 0;        //清标志位	
+		sendFlag = 0;        //清标志位
 	}
 	if(RI){
 		RI = 0;
-		String[posString++] = SBUF;	
+		String[posString++] = SBUF;
 		msgT = 0;
 		TR0 = 1;	//开始计时（确定是否接收完成）
 	}
@@ -286,11 +414,17 @@ void Uart2Send(unsigned char *Rec){
 	while(*Rec){
 		S2BUF = *Rec++;
 		DelayCharUsart();
-	}	
+	}
 }
 /*串口2中断*/
 void Uart2_Interrupt() interrupt 8 using 1{
 	if(S2CON & S2RI)											//接收到字节
+		if(S2BUF == 0x3D){
+			A4Pos = 0;
+			A4D1 = A4[5];
+			A4D0 = A4[6];
+		}
+		A4[A4Pos++] = S2BUF;
 		S2CON &= ~S2RI;
 	if(S2CON & S2TI)											//字节发送完毕
 		S2CON &= ~S2TI;										//手动清零发送中断标志位
